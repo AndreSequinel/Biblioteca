6e40a5671675edfe49059e6dfbecd4ea0877998a
André Sequinel <andresequinel@gmail.com>
2026-05-11 00:00:03 -0300

    Inclusa model, appDbContext e atualizacao gitIgnore

---
+++ Biblioteca/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Livro> Livros { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Livro>(entity =>
+        {
+            entity.Property(l => l.Titulo)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(l => l.Editora)
+                .HasMaxLength(100);
+        });
+    }
+}
---
+++ Biblioteca/Models/Livro.cs
@@ -0,0 +1,10 @@
+﻿namespace Biblioteca.Models;
+
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public int AnoDeLancamento { get; set; }
+    public string Editora { get; set; } = string.Empty;
+    public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+}
